bcf3d1559d529d0e0cb9cd4c83d00813daecfd84
coder-showkat <showkat.coder@gmail.com>
2023-05-02 00:36:06 +0600

    adding the route for chef api

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const Chefs = require("./chef.json");
+
 const app = express();
 const port = 5001;
 
@@ -12,6 +14,11 @@ app.get("/", (req, res) => {
     "<h1 style='text-align: center;'>Welcome to Chef's Table server</h1>"
   );
 });
+
+// chef api route
+app.get("/api/chef", (req, res) => {
+    res.send(Chefs);
+})
 
 app.listen(port, () => {
   console.log(`Server is running at port ${port}`);
